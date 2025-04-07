f3c93bc50420449ad9db1518038a7917619ce00f
Carlos Alegría <Ca9085079@gmail.com>
2025-04-07 11:43:47 -0500

    Dataset
    Creación del dataset

---
+++ Dataset.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# Cargar el dataset desde un archivo CSV
+df = pd.read_csv(r'C:\Users\Ca908\Downloads\archive\heart.csv')
+
+# Mostrar las primeras filas del dataset
+print(df.head())
